114159fd30d8f512a29eea3b2a1fb64c284d4520
tavisca-amans <Aman.Shrivastava@tavisca.com>
2022-03-11 16:36:16 +0530

    modified  filterLogs

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -5,7 +5,7 @@ import org.apache.commons.lang.StringUtils
 def call(String filter_string, int occurrence) {
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
-    if (count > occurrence -1) {
+    if (count > occurrence) {
         currentBuild.result='UNSTABLE'
     }
 }
